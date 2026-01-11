87cb894daaebd24daa811367bb894c26bf058764
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2026-01-11 17:40:28 +0200

    Create urls.py

---
+++ properties/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for properties app
+"""
+from django.urls import path
+from . import views
+
+app_name = 'properties'
+
+urlpatterns = [
+    path('', views.property_list, name='property_list'),
+]
